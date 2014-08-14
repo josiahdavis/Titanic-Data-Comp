d514ccbd21af777518839df55f48b253c24988d8
Josiah Davis <josiah.j.davis@gmail.com>
2014-08-14 08:11:35 -0400

    Added any lived variable

--- Titanic_Kaggle.R
+++ Titanic_Kaggle.R
@@ -1,12 +1,17 @@
 # Kaggle Competition
 rm(list = ls()); gc()
 set.seed(98143)
-library(caret, rpart, plyr)
-library(randomForest, ada, neuralnet)
-library(e1071, missForest)
+library(caret)
+library(rpart)
+library(party)
+library(plyr)
+library(randomForest)
+library(neuralnet)
+library(ada)
+library(e1071)
+library(missForest)
 
 # TO-DO: 
-# Save models within the for loop
 # Add spouse variable
 
 setwd("C:/Users/josdavis/Documents/GitHub/Titanic-Data-Comp")
@@ -19,9 +24,11 @@ train <- read.csv("train.csv", header = TRUE)
 
 # Want to determine the number of family members
 # Combine data sets 
+
+test$Survived <- 5 # Create the variable for now
 test$dat <- "test"
 train$dat <- "train"
-combined <- rbind(train[,-2], test)
+combined <- rbind(train, test)
 
 # Get last name
 combined$last.name <- strsplit(as.character(combined$Name), ",")
@@ -41,15 +48,26 @@ combined$Title <- as.factor(getTitle(combined))
 rm(getTitle)
 
 # Impute missing values (does not change order)
-combined.i <- missForest(combined[c("Pclass", "Sex", "Age",
+combined.i <- missForest(combined[c("Pclass", "Sex", "Age", "Parch",
                                   "SibSp", "Fare", "Embarked", "family.no", "Title")], verbose = FALSE)$ximp
 
-combined <- cbind(combined.i, combined[c("Name", "PassengerId", "dat")])
+combined <- cbind(combined.i, combined[c("Name", "PassengerId", "dat", "Survived", "Ticket", "Cabin")])
+rm(combined.i)
+
+# Did any of the passengers on the ticket live?
+combined <- ddply(combined, c("Ticket"), function(x)cbind(x, any.lived = any(x$Survived == 1)))
+
+# Did any of the passengers on the ticket die?
+combined <- ddply(combined, c("Ticket"), function(x)cbind(x, any.died = any(x$Survived == 0)))
 
 # Split the data back to original training and testing sets
 test <- combined[combined$dat == "test",]
-train <- merge(combined[combined$dat == "train",], train[,c("Survived", "Name")],
-                 by = "Name", all.x = TRUE, all.y = TRUE)
+train <- combined[combined$dat == "train",]
+
+# train <- merge(combined[combined$dat == "train",], train[,c("Survived", "Name")],
+#                  by = "Name", all.x = TRUE, all.y = TRUE)
+
+rm(combined)
 
 # Create a numeric version as well
 # train.n <- train[c("Survived", "Pclass", "Sex", "Age",
@@ -65,13 +83,13 @@ iterations = 5
 results = data.frame(matrix(0, 5 * iterations, 4, dimnames = 
                               list(NULL, c("Model", "Accuracy", "Sensitivity", "Specificity"))))
 
+el <- vector("list", iterations)
 
-models <- vector(mode="list", length=iterations)
-models <- list(logit = models, 
-               forest = models, 
-               boost = models,
-               nb = models,
-               ensemble = models)
+models <- list(logit = el, 
+               forest = el, 
+               boost = el,
+               nb = el,
+               ensemble = el)
 
 
 for(i in 1:iterations){
@@ -87,10 +105,10 @@ for(i in 1:iterations){
   #################
   # Logit
   #################
-  m.logit <- glm(Survived~Pclass+Sex+Age+SibSp+Fare+family.no, family = binomial(logit), data = train.1)
+  m.logit <- glm(Survived~Pclass+Sex+Age+SibSp+Fare+family.no, family = binomial(logit), data = train)
   models$logit[[i]] <- m.logit
-  p.logit <- predict(m.logit, type = "response", newdata = train.2)
-  r <- confusionMatrix(round(p.logit, 0), train.2$Survived)
+  p.logit <- predict(m.logit, type = "response", newdata = train)
+  r <- confusionMatrix(round(p.logit, 0), train$Survived)
   results[i,] <- c("Logit", r$overall[1], r$byClass[1], r$byClass[2])
   rm(r, m.logit)
   
@@ -105,17 +123,25 @@ for(i in 1:iterations){
   results[iterations+i,] <- c("Random Forest", r$overall[1], r$byClass[1], r$byClass[2])
   rm(r, m.forest)
   
+  #################
+  # Conditional Inference Forest
+  ##################
+  m.cforest <- cforest(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + 
+                         Fare + Embarked + family.no, data = train)
+  p.cforest <-  predict(m.cforest, newdata = train.2)
+  r <- confusionMatrix(p.cforest, train.2$Survived) 
+  
   ##################
   # Boosted Trees
   ##################
   m.boost <-ada(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + 
-                  Fare + Embarked + family.no, data=train.1, verbose=TRUE,na.action=na.rpart)
+                  Fare + Embarked + family.no, data=train, verbose=TRUE,na.action=na.rpart)
   models$boost[[i]] <- m.boost
-  p.boost <-predict(m.boost, newdata=train.2, type="vector")
-  r <- confusionMatrix(p.boost, train.2$Survived)
+  p.boost <-predict(m.boost, newdata=train, type="vector")
+  r <- confusionMatrix(p.boost, train$Survived)
   results[iterations*2+i,] <- c("Boosted Trees", r$overall[1], r$byClass[1], r$byClass[2])
   rm(r, m.boost)
-#   
+   
 #   ##################
 #   # Neural Networks
 #   ##################
@@ -128,7 +154,7 @@ for(i in 1:iterations){
   ##################
   # Naive Bayes 
   ##################
-  m.nb <- naiveBayes(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + Fare + Embarked + family.no, data = train.1)
+  m.nb <- naiveBayes(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + Fare + Embarked + family.no, data = train)
   models$nb[[i]] <- m.nb
   p.nb<-predict(m.nb,newdata=train.2)
   r <- confusionMatrix(p.nb, train.2$Survived)
@@ -136,7 +162,7 @@ for(i in 1:iterations){
   rm(r, m.nb)
   
   ##################
-  # Ensemble Model 
+  # Ensemble Model (Random Forest)
   ##################
   pred <- data.frame(Survival=train.2$Survived,
                      logit=round(p.logit, 0), 
@@ -149,7 +175,7 @@ for(i in 1:iterations){
   r <- confusionMatrix(p.ensemble, pred$Survival)
   results[iterations*4+i,] <- c("Ensemble", r$overall[1], r$byClass[1], r$byClass[2])
   rm(r, pred, m.ensemble, p.ensemble, p.logit, p.boost, p.forest, p.nb)
-  
+
 }
 
 rm(i, formula)
@@ -160,18 +186,36 @@ results$Specificity <- round(as.numeric(results$Specificity),4)
 results
 
 results.avg <- ddply(results, ~Model, summarise, avg = round(mean(Accuracy), 5))
+results.avg[order(results.avg$avg, decreasing = TRUE),]
 
 #################
 # Make the final preditions
 #################
 
-test$logit <- round(predict(models$logit[[3]], type = "response", newdata = test),0)
-test$rf <- predict(models$forest[[3]], newdata = test)
-test$bt <- predict(models$boost[[3]], newdata = test)
-test$nb <- predict(models$nb[[3]], newdata = test)
+test$logit <- round(predict(models$logit[[1]], type = "response", newdata = test),0)
+test$rf <- as.numeric(predict(models$forest[[1]], newdata = test))-1
+test$bt <- as.numeric(predict(models$boost[[1]], newdata = test))-1
+test$nb <- as.numeric(predict(models$nb[[1]], newdata = test))-1
 
 test$Survived <- predict(models$ensemble[[1]], 
                          newdata = test[c("logit", "rf", "bt","nb")])
 
+test$Survived <- round(predict(m.logit, type = "response", newdata = test),0) # Trying a single logit model
+test$Survived <- predict(m.cforest, newdata = test) # Trying a single rf model
+
 submission <- test[c("PassengerId", "Survived")]
 write.csv(submission, "VICE.csv", row.names = FALSE)
+
+tuneRF <- train(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + 
+                  Fare + Embarked + family.no, method = "rf", train, 
+                  trControl = trainControl(method = "cv", number = 10),
+                  metric = "Accuracy")
+names(tuneRF)
+m.forest <- tuneRF$finalModel
+test$Survived <- predict(tuneRF$finalModel, newdata = test) # Trying a tuned RF
+
+pred <- data.frame(Survival=train$Survived,
+                   logit=round(p.logit, 0), 
+                   bt=as.numeric(p.boost)-1, 
+                   rf=as.numeric(p.forest)-1)
+test$Survived <- ifelse(rowSums(test[c("logit", "bt", "rf")])>1, 1, 0) # TRying a simple average
